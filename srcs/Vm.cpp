1dcf0248fcffba81f18cd93560a02dfb51dc6960
fenixD3 <filipcev2@gmail.com>
2021-04-10 17:06:02 +0300

    fix runtime exception

--- srcs/Vm.cpp
+++ srcs/Vm.cpp
@@ -43,6 +43,7 @@ void Vm::Process()
             catch (const VmException& aError)
             {
                 mErrorManager->AddError(aError.what());
+				throw ErrorManagerException("");
             }
         }
         if (!mErrorManager->IsEmptyError())
@@ -191,6 +192,7 @@ std::ostream& Vm::WriteError(std::ostream& outErrorStream)
 
 bool Vm::CheckDivisionByZero(const IOperand* aOperand) const
 {
+	auto i = std::stoi(aOperand->toString());
 	if ((aOperand->getType() < eOperandType::Float && std::stoi(aOperand->toString()) == 0)
 		|| (aOperand->getType() == eOperandType::Float && IsEqualTwoFloating(std::stof(aOperand->toString()), 0.0f, MaxULPS))
 		|| ((aOperand->getType() == eOperandType::Double && IsEqualTwoFloating(std::stod(aOperand->toString()), 0.0, MaxULPS))))
